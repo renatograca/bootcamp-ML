97d2692bb3caa6a28013efacb0e913f97a23a8a2
renatograca <renatomacedo1995@gmail.com>
2022-03-17 11:34:07 -0300

    Hello World em Maven

---
+++ 05-maven-project/first-project/src/test/java/com/bootcamp/meli/AppTest.java
@@ -0,0 +1,20 @@
+package com.bootcamp.meli;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
